cc758ca31cb93c85e5abe0fa999a010bfbed8359
jaLeague <jackie.arseneau@jointheleague.org>
2017-11-07 19:21:44 -0800

    animalfarm

--- src/methods/AnimalFarm.java
+++ src/methods/AnimalFarm.java
@@ -5,6 +5,7 @@ package methods;
 
 
 import java.io.File;
+import java.nio.channels.ShutdownChannelGroupException;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -18,9 +19,31 @@ AnimalFarm() {
 	 * 1. Ask the user which animal they want, then play the sound of that
 	 * animal.
 	 */
-
+	String light = JOptionPane.showInputDialog("what animal you want to play a noice UGH");
 	/* 2. Make it so that the user can keep entering new animals. */
+if (light.equals("wolf")){
+	playWoof();
 }
+	
+else if (light.equals("cow")){
+	playMoo();
+}
+else if (light.equals("DUCK")){
+	playQuack();
+}
+
+else if (light.equals("LLama")){
+	playLLama();
+}else if (light.equals("Kitty")){
+	 playKittyCat();
+}
+
+
+ 
+else {
+	JOptionPane.showMessageDialog(null, "BACK AWAY FROM THE COMPUTER NOW, or you are going to die right now or you just run away.No you are going to get the code right now or you are my servent FOREVER IN YOUR LIFE.DIE NOW or sufer the conseces");}
+}
+
 
 void playMoo() {
 	playNoise(mooFile);
@@ -34,6 +57,13 @@ void playWoof() {
 	playNoise(woofFile);
 }
 
+void playLLama() {
+	playNoise(llamaFile);
+}
+
+void playKittyCat() {
+	playNoise(meowFile);
+}
 String quackFile = "quack.wav";
 String mooFile = "moo.wav";
 String woofFile = "woof.wav";
