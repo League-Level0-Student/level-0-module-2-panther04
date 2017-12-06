df315205c88783dbf1b5cd14ea51305955374768
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-05 19:10:21 -0800

    that was easy string methads 1

--- src/extra/StringMethods1.java
+++ src/extra/StringMethods1.java
@@ -11,17 +11,28 @@ public class StringMethods1 {
 String yelnats = "YOU WILL DIE!!!!!!!!Not know but soon Don't panick you have a lot more to go";
 // 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
+
+System.out.println(yelnats.charAt(3));
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
+System.out.println(yelnats.length());
 
 		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
+		//    HINT: use .length() to determine how many loop
+for (int i = 0; i < yelnats.length(); i++) {
+	System.out.println(yelnats.charAt(i));
+}
 
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
+String stanley = JOptionPane.showInputDialog("enter a charater");
+char nj=stanley.charAt(0);
+for (int i = 0; i < yelnats.length(); i++) {
+	nj = yelnats.charAt(0);
+}
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
